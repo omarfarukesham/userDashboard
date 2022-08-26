5fa609930eb906b24559e80980a7b65bd150eabd
Omar Faruk <omarfarukesham@gmail.com>
2022-08-26 23:41:08 +0600

    Routes has working perfectly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
+import Users from './Components/Users';
+import Analysis from './Components/Analysis';
+import CustomiseUser from './Components/CustomiseUser';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
-       <Home></Home>
+      <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='user' element={<Users></Users>}></Route>
+          <Route path='analysis' element={<Analysis></Analysis>}></Route>
+          <Route path='customizeData' element={<CustomiseUser></CustomiseUser>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,14 +11,17 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </label>
                         <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Homepage</a></li>
-                            <li><a>Portfolio</a></li>
-                            <li><a>About</a></li>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/analysis'>Analysis</Link></li>
+                            <li><Link to="/customizeData">CustomizeData</Link></li>
                         </ul>
                     </div>
                 </div>
                 <div class="navbar-center">
                     <Link to='/' class="btn btn-ghost normal-case text-xl">Dashboard</Link>
+                    <Link to='/user' class="btn btn-ghost normal-case text-xl">User</Link>
+                    <Link to='/analysis' class="btn btn-ghost normal-case text-xl">Analysis</Link>
+                    <Link to='/customizeData' class="btn btn-ghost normal-case text-xl">CustomizeData</Link>
                 </div>
                 <div class="navbar-end">
                     <button class="btn btn-ghost btn-circle">
