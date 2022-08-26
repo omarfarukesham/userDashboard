f368f67c49a31e80895612ac1b54cfa880eb0f93
Omar Faruk <omarfarukesham@gmail.com>
2022-08-27 00:14:39 +0600

    user data has loaded from MongoDB

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+
+        fetch('http://localhost:5000/master')
+            .then(res => res.json())
+            .then(data => setUsers(data.slice(0, 15)))
+
+    }, [])
+
     return (
-        <div>
-            <div class="card w-96 bg-base-100 shadow-xl image-full">
-                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                <div class="card-body">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
+        <>
+
+
+            <h1 className='text-center text-2xl'>User Data = {users.length}</h1>
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 p-5'>
+                {
+                    users.map((pd, index) =>
+                        <div key={pd._id} class="card card-compact w-56 bg-base-200 shadow-xl gap-4 m-2">
+                            <figure><img src={pd?.img} alt="Shoes" /></figure>
+                            <div class="card-body">
+                                <h2 class="card-title">User -{pd?.Nmae}</h2>
+                                <small className='font-bold text-primary'>Country :{pd?.Country}</small>
+                                <p>Device :{pd?.Device }</p>
+                                <p>Sex :{pd?.gender}</p>
+                                <div class="card-actions justify-around">
+                                    <label  class="btn btn-primary  modal-button btn-xs text-white">Details</label>
+                                    {/* <button for="my-modal-6" class="btn btn-primary  modal-button btn-xs text-white">Details</button> */}
+                                    <button  class="btn btn-error btn-xs text-white">Delete</button>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    )
+                }
+
+
             </div>
-        </div>
+
+           
+        </>
     );
 };
 
