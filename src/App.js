cd7a737202674999fbbc4376d5cb93c7f0225763
Omar Faruk <omarfarukesham@gmail.com>
2022-08-26 22:21:18 +0600

    tailwind, react-router-dom, dasiyUI set up done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar></Navbar>
        <Home></Home>
     </div>
   );
